f8fc97b72adf94a9c9cfaf2739ace4eab51aed48
Rune Nielsen <runenielsen@runbox.com>
2021-05-24 00:42:29 +0200

    initial skeleton

---
+++ cmd/hosts.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var hostsCmd = &cobra.Command{
+	Use:   "hosts",
+	Short: "Add hosts with the add command",
+	Long: `Manages the hosts list for pScan
+
+Add hosts with the add command
+Delete hosts with the delete command
+List hosts with the list command.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hosts called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(hostsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// hostsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// hostsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/root.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "pScan",
+	Short: "Fast TCP port scanner",
+	Long: `pScan - short for Port Scanner - executes TCP port scan on a list of hosts.
+
+pScan allows you to add, list, and delete hosts from the list.
+
+pScan executes a port scan on specified TCP ports. You can customize the target ports using a command line flag.`,
+	Version: "0.1",
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pScan.yaml)")
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".pScan" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".pScan")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
